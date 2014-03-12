aff0acae972ced080e2e68cfe95feb102c35d75f
Patrick Perey <the4dpatrick@yahoo.com>
2014-03-12 16:21:28 -0700

    Define find_by_incomplete in task model

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,9 +1,9 @@
 class TaskController < ApplicationController
   def incomplete
-    @tasks = Task.find_all_by_complete(false)
+    @tasks = Task.find_by_incomplete
   end
 
   def last_incomplete
-    @tasks = Task.find_by_complete(false, order: 'created_at DESC')
+    @tasks = Task.find_by_incomplete
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ActiveRecord::Base
+  def self.find_by_incomplete
+    find_by_complete(false, order: 'created_at DESC')
+  end
 end
